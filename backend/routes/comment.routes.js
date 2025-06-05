817afe098f2eb04060a4d3bc9f95791ca9cb9a41
Nikhil Dhaliya <nikhil11754@gmail.com>
2025-06-06 02:36:23 +0530

    removed setusername from routes

--- backend/routes/comment.routes.js
+++ backend/routes/comment.routes.js
@@ -7,7 +7,6 @@ import {
     getCommentsByUser
 } from '../controllers/comment.controller.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { setUsernameMiddleware } from '../middlewares/setUsername.js';
 
 const router = express.Router();
 
@@ -15,7 +14,6 @@ const router = express.Router();
 router.get('/posts/:postId', getComments);
 
 // Protected routes
-router.use(authMiddleware, setUsernameMiddleware);
 router.post('/posts/:postId', createComment);
 router.post('/:commentId/vote', voteComment);
 router.delete('/:commentId', deleteComment);
