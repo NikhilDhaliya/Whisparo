e4b8ee780bc8f55c7533b47a00ea3a8529a92cff
Nikhil Dhaliya <nikhil11754@gmail.com>
2025-06-05 23:11:01 +0530

    tailwind

--- fron/vite.config.js
+++ fron/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     server: {
       port: 5173,
       proxy: {
